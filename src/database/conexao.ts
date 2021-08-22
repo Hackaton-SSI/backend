ed414ed4379c8620d3f0df8a34a8a9f29c76c5c9
vinihcrosa <vinihcrosa@users.noreply.github.com>
2021-08-22 07:05:19 -0300

    Update conexao.ts

--- src/database/conexao.ts
+++ src/database/conexao.ts
@@ -1,5 +1,23 @@
 import { createConnection } from "typeorm"
 
-createConnection().then(() => {
+createConnection({
+  type: "postgres",
+  host: process.env.DB_HOST || "localhost",
+  port: 5432,
+  username: process.env.DB_USER || "postgres",
+  password: process.env.DB_PASS || "123456789",
+  database: process.env.DB_DATABASE || "hackaton",
+  migrations: ["./src/database/migrations/*ts"],
+  entities: ["./src/models/*ts"],
+  cli: {
+    migrationsDir: "./src/database/migrations"
+  },
+  ssl: true,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false
+    }
+  }
+}).then(() => {
   console.log('🎲 Connexão feita')
 })
